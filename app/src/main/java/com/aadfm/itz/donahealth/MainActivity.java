cd98fb63c329417f215a7f96fe1a28b45d56f71e
gitProyecto <floresmiranda_15@hotmail.com>
2015-11-06 13:09:03 -0600

    commit 1

--- app/src/main/java/com/aadfm/itz/donahealth/MainActivity.java
+++ app/src/main/java/com/aadfm/itz/donahealth/MainActivity.java
@@ -1,13 +1,61 @@
 package com.aadfm.itz.donahealth;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        img = (ImageView) findViewById(R.id.imageView);
+        resizeImage(MainActivity.this,R.id.imageView,100,100);
+
+
     }
+
+
+    public static Drawable resizeImage(Context ctx, int resId, int w, int h) {
+
+        // cargamos la imagen de origen
+        Bitmap BitmapOrg = BitmapFactory.decodeResource(ctx.getResources(),resId);
+
+        int width = BitmapOrg.getWidth();
+        int height = BitmapOrg.getHeight();
+        int newWidth = w;
+        int newHeight = h;
+
+        // calculamos el escalado de la imagen destino
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+
+        // para poder manipular la imagen
+        // debemos crear una matriz
+
+        Matrix matrix = new Matrix();
+        // resize the Bitmap
+        matrix.postScale(scaleWidth, scaleHeight);
+
+        // volvemos a crear la imagen con los nuevos valores
+        Bitmap resizedBitmap = Bitmap.createBitmap(BitmapOrg, 0, 0,width, height, matrix, true);
+
+        // si queremos poder mostrar nuestra imagen tenemos que crear un
+        // objeto drawable y así asignarlo a un botón, imageview...
+        return new BitmapDrawable(resizedBitmap);
+
+    }
+
+
+
 }

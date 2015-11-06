2dfe793da35731e9437aeccc7672a94473e94f2d
gitProyecto <floresmiranda_15@hotmail.com>
2015-11-06 14:04:41 -0600

    Creación de interfaz tictac

---
+++ app/src/main/java/com/aadfm/itz/donahealth/TicTae.java
@@ -0,0 +1,13 @@
+package com.aadfm.itz.donahealth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TicTae extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tic_tae);
+    }
+}
